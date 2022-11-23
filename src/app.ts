39c4c92ed39f2a61a304b9589b9b661b28c05213
Fábio <fabio@arquivarnet.com.br>
2022-11-23 16:51:05 -0500

    update

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(
 app.use(express.json());
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'http://localhost:5174'],
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     credentials: true,
   }),
